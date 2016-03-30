163d5719c7e1d93949055fdfd8e6af75b08a1fde
Edy Syuryawan Saputra <syuryawanedy@gmail.com>
2016-03-30 11:10:02 +0700

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,3 +1,20 @@
-
-
-System.out.print"Masukkan nama : ";
+import javax.swing.JOptionPane;
+public class Fibonacci
+{
+ public static void main(String[]args)
+ {
+  int a = Integer.parseInt(JOptionPane.showInputDialog("Masukkan nilai awal Deret Fibonacci"));
+  int b = a;
+  int c = Integer.parseInt(JOptionPane.showInputDialog("Masukkan deret Fibonacci yang Anda Inginkan: "));
+  int d = c-1;
+  int e = 1;
+  System.out.print(a + " ");
+  while(e <=d)
+  {
+   System.out.print(a +" ");
+   a = a+b;
+   b = a-b;
+   e++;
+  }
+ }
+}
